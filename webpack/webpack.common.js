4bb036f3429cba5be13c78f222f6c0a84b88bd31
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-11-17 23:20:45 -0800

    feat: Add evmts packages

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { webpackPluginEvmts } = require('@evmts/webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
@@ -14,14 +15,26 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'],
     fallback: {
       stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer'),      
+      buffer: require.resolve('buffer'),
       process: require.resolve("process/browser"),
       'process/browser': require.resolve('process/browser')
-      
+
     }
   },
   module: {
-    rules: [{ test: /\.tsx?$|\.jsx?$/, include: path.join(__dirname, '../src'), loader: 'ts-loader' }]
+    rules: [
+      {
+        test: /\.tsx?$|\.jsx?$/,
+        include: path.join(__dirname, '../src'),
+        loader: 'ts-loader',
+        options: {
+          // when @evmts/tsc is complete can use that instead of transpile only to do typechecking
+          // @see ✨ feat: Add evmts-tsc https://github.com/evmts/evmts-monorepo/pull/446
+          // compiler: '@evmts/tsc',
+          transpileOnly: true
+        }
+      }
+    ]
   },
   optimization: {
     splitChunks: {
@@ -47,7 +60,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.DEBUG': JSON.stringify('eth-js')
     }),
-
+    webpackPluginEvmts(),
   ],
   experiments: {
     topLevelAwait: true

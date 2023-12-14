54e428693120e2466bcf67c262385c94e1e763e7
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-13 17:19:24 -0800

    fix: Upgrade tevm to latest and fix contract compile bug

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { webpackPluginEvmts } = require('@evmts/webpack-plugin')
+const { webpackPluginTevm } = require('@tevm/webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = {
@@ -28,9 +28,9 @@ module.exports = {
         include: path.join(__dirname, '../src'),
         loader: 'ts-loader',
         options: {
-          // when @evmts/tsc is complete can use that instead of transpile only to do typechecking
-          // @see ✨ feat: Add evmts-tsc https://github.com/evmts/evmts-monorepo/pull/446
-          // compiler: '@evmts/tsc',
+          // when @tevm/tsc is complete can use that instead of transpile only to do typechecking
+          // @see ✨ feat: Add tevm-tsc https://github.com/tevm/tevm-monorepo/pull/446
+          // compiler: '@tevm/tsc',
           transpileOnly: true
         }
       }
@@ -60,7 +60,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.DEBUG': JSON.stringify('eth-js')
     }),
-    webpackPluginEvmts(),
+    webpackPluginTevm(),
   ],
   experiments: {
     topLevelAwait: true
